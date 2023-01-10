d6a014ecf8f8b394da2b8b824a235d1a9dad0433
exion pc <jescamilla1@gmail.com>
2023-01-09 22:07:09 -0500

    removed extra image files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import logo from '../images/sp.png'
+import React from 'react';
+import logo from '../images/Spotify_Logo_CMYK_Black.png';
 function Header() {
   return (
-    <div>
-        <img href={logo} alt="spotify logo"/><h2 className='spotify-font'>Spotify Playlist Editor</h2>
+    <div className="header-container">
+        <img className="logo" src={logo} alt="spotify logo"/><h2 className='spotify-font'>Playlist Editor</h2>
     </div>
   )
 }
